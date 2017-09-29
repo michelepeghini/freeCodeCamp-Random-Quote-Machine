7a83c26f720f2cab2d758aaa32ebfd888dc23142
michelepeghini <michelepeghini@gmail.com>
2017-09-29 15:22:26 +1000

    final polish

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* NOTE: Wikiquote API has some glitches and at times it breaks, if the quotes stops updating, that's why.*/
+/* NOTE: Wikiquote API (third aprty code) has some glitches and at times it breaks due to TypeError, I didn't have time to try to fix it... If the quotes stops updating, that's why.*/
 
 // pre defined set of colors for #quote_box color
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
@@ -7,7 +7,7 @@ var timer; // stores interval for random generation of quote
 
 //returns a color extracted randomly from colors array
 function getRandColor (colors) {
-  var randC = Math.floor(Math.random()*colors.length)
+  var randC = Math.floor(Math.random()*colors.length);
   return colors[randC];
 }
 
@@ -38,7 +38,7 @@ function changeColor () {
   });
 }
 
-// uodates anchor link to twitter with current quote text
+// updates anchor link to twitter with current quote text
 function updateTwitterButton(text) {
   $('#twitter_button').attr('href', 'https://twitter.com/intent/tweet?text=' + text);
 }
@@ -47,10 +47,19 @@ function updateTwitterButton(text) {
 function getNewQuote() {
   WikiquoteApi.queryRandomTitle(
     function(data) {
-      WikiquoteApi.getRandomQuote(data, function(data) {
-        clearInterval(timer);
-        displayQuote(data);
-      });
+      try {
+        WikiquoteApi.getRandomQuote(data, function(data) {
+          clearInterval(timer);
+          displayQuote(data);
+          timer = setTimeout( getNewQuote , interval*1000 );
+        });  
+      } catch(e) {
+        if (e instanceof TypeError) {
+          console.log(e);
+          clearInterval(timer);
+          timer = setTimeout( getNewQuote , interval*1000 );
+        }
+      } 
     },
     function(msg) {
       alert.log(msg);
@@ -66,28 +75,33 @@ function displayQuote (quote) {
   } else {
   $('#author span').text(quote.titles);
   }
-  resizeFonts(quote.quote.length);
+  try{
+    resizeFonts(quote.quote.length);  
+  } catch(e) {
+    console.log(e);
+  }
   updateTwitterButton(quote.quote);
   changeColor();
   timer = setTimeout( getNewQuote , interval*1000 );
   $('#quote_box span, #author, #twitter_button').show();
 }
 
-// event listener for get new quote button, queries for a new quote
-$('#newQuote').on('click', function() {
-	$('#quote_box span, #author').hide();
-	getNewQuote();
-});
-
-//  event listener for change color button, changes color of quote box
-$('#changeColor').on('click', function() {
-	changeColor(); 
-});
-
 // on document ready, hides coontent of quote box and queries for a quote
 $(document).ready(function() {
+  // event listener for get new quote button, queries for a new quote
+  $('#newQuote').on('click', function() {
+    $('#quote_box span, #author').hide();
+    getNewQuote();
+  });
+
+  //  event listener for change color button, changes color of quote box
+  $('#changeColor').on('click', function() {
+    console.log('before');
+    changeColor(); 
+    console.log('after');
+  });
+
+  
 	$('#quote_box span, #author, #twitter_button').hide();
 	getNewQuote();
-	console.log('teast');
-	
 });
